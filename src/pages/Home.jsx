f8b985a39de7de50e300528e3a63ea905dd55330
ShahidMomin777 <shahidmominc11@gmail.com>
2024-09-20 01:32:20 +0530

    created router dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/starred">Go to starred. </Link>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>
+       star page
+    </div>
+  )
+}
+
+export default Starred
